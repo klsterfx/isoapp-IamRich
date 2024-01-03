6cfcb8435079d21c4253eba991b2b6eeeb82db21
Kush Jayesh Ahir <ahir.kush1649@gmail.com>
2024-01-03 10:41:36 -0700

    Add files via upload

---
+++ i am Rich/i am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am Rich
+//
+//  Created by Kay Ahir on 1/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
